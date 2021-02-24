2bfa62806ac38cb45d51146426508f7b6dd49f4e
Giovanni Aranda <giovanni.arandaa@gmail.com>
2021-02-23 21:06:33 -0600

    Travis config

---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/providerMock';
+import ProductMock from '../../__mocks__/ProductMock';
+import Product from '../../components/Product';
+
+describe('<Product />', () => {
+  test('Render del componente Produc', () => {
+    const product = shallow(<ProviderMock><Product /></ProviderMock>);
+    expect(product.length).toEqual(1);
+  });
+
+  test('Comprobar el boton de comprar', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product product={ProductMock} handleAddToCart={handleAddToCart} />
+      </ProviderMock>,
+    );
+
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+});
